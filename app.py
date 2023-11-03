43baa800eb36b9e949498e3c21b16db9c7a7f91f
Zman82-dot <dorian.r.coleman@gmail.com>
2023-11-02 23:20:27 -0400

    added image and advanced search

--- app.py
+++ app.py
@@ -8,8 +8,13 @@ app = Flask("My Flask Application")
 def renderIndexPage():
     return render_template('index.html')
     
+@app.route("/image_search.html")
+def renderimage_search():
+    return render_template('image_search.html')
 
-
+@app.route("/advanced_search")
+def renderadvanced_search():
+    return render_template('advanced_search.html')
 
 if __name__=="__main__":
     app.run(debug=True) 
